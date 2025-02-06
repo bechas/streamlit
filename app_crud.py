cad18320a4ca82bb22e914683f4fdba7abd20330
Rafael Bechelli <125463026+bechas@users.noreply.github.com>
2025-02-06 16:45:01 -0300

    Update app_crud.py

--- app_crud.py
+++ app_crud.py
@@ -13,7 +13,7 @@ def get_db_connection():
         port="5432"         # porta do PostgreSQL
         )
         return con
-    except OperationalError as e:
+    except Exceotion as e:
         st.error(f"Erro ao conectar ao banco de dados: {e}")
         return None
 
